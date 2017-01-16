a23d868b0856a916de8d43d134d37906cac1e6a4
lenglang <364127728@qq.com>
2017-01-16 17:56:49 +0800

    获取动画时长、设定层级、单例、动画播放次数获取

---
+++ Assets/WZK/Scripts/Animator/AnimatorTool.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorTool {
+    /// <summary>
+    /// 获取某段动画长度
+    /// </summary>
+    /// <param name="animator"></param>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static float GetAnimationLenghtByName(Animator animator, string name)
+    {
+        RuntimeAnimatorController runtimeAnimatorController = animator.runtimeAnimatorController;
+        AnimationClip[] clips = runtimeAnimatorController.animationClips;
+
+        foreach (var clip in clips)
+        {
+            if (clip.name.Equals(name))
+            {
+                return clip.length;
+            }
+        }
+        return 0;
+    }
+    
+}
